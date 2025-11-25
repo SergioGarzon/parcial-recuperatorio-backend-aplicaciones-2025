3e9f36cb98859e95e1b3063f366ef628fee796ce
Sergio Garzon <sggarzonutn@gmail.com>
2025-11-24 23:14:13 -0300

    feat: Modifico un poco el archivo de lectura del csv, el archivo .java

--- src/main/java/utnfrc/isi/back/csv/CsvImporter.java
+++ src/main/java/utnfrc/isi/back/csv/CsvImporter.java
@@ -9,15 +9,20 @@ public class CsvImporter {
     public static void importArtists(EntityManager em, String filePath) {
         try {
             List<ArtistCsv> artistCsvs = CsvReader.readArtists(filePath);
+            
             em.getTransaction().begin();
+
             for (ArtistCsv artistCsv : artistCsvs) {
                 Artist artist = new Artist();
                 artist.setIdArtist(artistCsv.getArtistId());
                 artist.setName(artistCsv.getName());
                 em.merge(artist); // Usar merge() en lugar de persist()
             }
+
             em.getTransaction().commit();
+
             System.out.println("Artistas importados correctamente.");
+            
         } catch (Exception e) {
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
