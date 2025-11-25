9cd6bd4836d652d49d746710b059403b85f45772
Sergio Garzon <sggarzonutn@gmail.com>
2025-11-24 21:13:33 -0300

    feat: Proyecto funcionando ok

--- src/main/java/utnfrc/isi/back/App.java
+++ src/main/java/utnfrc/isi/back/App.java
@@ -1,7 +1,10 @@
 package utnfrc.isi.back;
 
 import utnfrc.isi.back.domain.Artist;
+import utnfrc.isi.back.domain.Genre;
+import utnfrc.isi.back.domain.MediaType;
 import utnfrc.isi.back.domain.Album;
+import utnfrc.isi.back.domain.Track;
 import utnfrc.isi.back.infra.LocalEntityManagerProvider;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
@@ -9,6 +12,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.List;
+
 
 /**
  * Proyecto para el parcial de BackEnd de Aplicaciones
@@ -87,6 +91,12 @@ public class App {
                     else 
                         System.out.println("\nSe debe crear primero la base de datos, poner opción 1");
                     break; 
+                case 7:
+                    if(control) 
+                        cargarTrack();                    
+                    else 
+                        System.out.println("\nSe debe crear primero la base de datos, poner opción 1");
+                    break;
                 case 9:
                     LocalEntityManagerProvider.close();
                     System.out.println("\nHasta luego");
@@ -103,13 +113,14 @@ public class App {
     private static void mostrarBD() {
         try {
             org.h2.tools.Server.createWebServer("-webPort", "8082", "-webAllowOthers").start();
-            System.out.println("\nConsola de H2 disponible en: http://localhost:8082");
-            System.out.println("\nRecorda las credenciales para acceder a la base de datos \n");
-            System.out.println("Controlador: org.h2.Driver");
-            System.out.println("URL JDBC: jdbc:h2:mem:backdb");
-            System.out.println("Nombre de usuario: sa");
-            System.out.println("Contraseña: (Ninguna)");
-            System.out.println("\n");
+            System.out.println("""
+                \nConsola de H2 disponible en: http://localhost:8082
+                Recorda las credenciales para acceder a la base de datos \n
+                Controlador: org.h2.Driver
+                URL JDBC: jdbc:h2:mem:backdb
+                Nombre de usuario: sa
+                Contraseña: (Ninguna)
+                \n""");
         } catch (Exception e) {
             System.err.println("\nNo se pudo iniciar el servidor web de H2: " + e.getMessage());
             e.printStackTrace();
@@ -201,4 +212,152 @@ public class App {
             e.printStackTrace();
         } 
     }    
+
+    private static void cargarTrack() throws IOException {
+        try {
+            System.out.println("\nPara cargar datos de los Track, primero se deben cargar por defecto algunos Genres y MusicTypes");
+
+            // Carga de datos de las tablas por separado
+            cargaGenres();
+            cargaMediaType();
+            
+            // Muestro los datos de los generos
+            Query query1 = em.createQuery("SELECT g FROM Genre g", Genre.class);
+            List<Genre> genreList = query1.getResultList();
+
+            System.out.println("\nLista de genres en la base de datos:");
+            
+            for (Genre g : genreList) {
+                System.out.println("ID: " + g.getIdGenre() + ", Nombre: " + g.getName());
+            }     
+
+            // Muestro los datos de los MediaTypes
+            Query query2 = em.createQuery("SELECT m FROM MediaType m", MediaType.class);
+            List<MediaType> mediaTypeList = query2.getResultList();
+
+            System.out.println("\nLista de MediaTypes en la base de datos:");
+            
+            for (MediaType m : mediaTypeList) {
+                System.out.println("ID: " + m.getIdMediaType() + ", Nombre: " + m.getName());
+            }     
+
+            // Muestro los datos de los Albums
+            Query query3 = em.createQuery("SELECT al FROM Album al", Album.class);
+            List<Album> albumData = query3.getResultList();
+
+            System.out.println("\nLista de Albums en la base de datos:");
+            
+            for (Album ad : albumData) {
+                System.out.println("ID: " + ad.getIdAlbum() + ", Nombre: " + ad.getTitle() + ", Número de artista: " + ad.getIdArtist());
+            }  
+            
+            Track track = new Track();
+
+            String nombreTrack = "", composerName = "";
+            int codigoAlbum = 0, codigoMediaType = 0, codigoGenre = 0, durationTrack = 0, byteTrack = 0;
+            float unitPrice = 0f;
+        
+            System.out.print("\nIngrese el nombre del track: ");
+            nombreTrack = entrada.readLine();
+
+            track.setName(nombreTrack);
+
+            do {
+                System.out.print("Ingrese el código del album: ");
+                codigoAlbum = Integer.parseInt(entrada.readLine());
+
+                for (Album al : albumData) {
+                    if(al.getIdAlbum() == codigoAlbum)
+                        track.setAlbum(al);
+                    else 
+                        codigoAlbum = 0;                  
+                } 
+
+                if(codigoAlbum == 0)
+                    System.out.println("Código de album incorrecto!");
+            } while(codigoAlbum == 0);
+
+            do {
+                System.out.print("Ingrese el código del MediaType: ");
+                codigoMediaType = Integer.parseInt(entrada.readLine());
+
+                for (MediaType mt : mediaTypeList) {
+                    if(mt.getIdMediaType() == codigoMediaType)
+                        track.setMediaTypeId(mt);
+                    else 
+                        codigoMediaType = 0;                  
+                } 
+
+                if(codigoMediaType == 0)
+                    System.out.println("Código de MediaType incorrecto!");
+            } while(codigoMediaType == 0);
+
+            do {
+                System.out.print("Ingrese el código del Genre: ");
+                codigoGenre = Integer.parseInt(entrada.readLine());
+
+                for (Genre gnr : genreList) {
+                    if(gnr.getIdGenre() == codigoGenre)
+                        track.setGenreId(gnr);
+                    else 
+                        codigoGenre = 0;                  
+                } 
+
+                if(codigoGenre == 0)
+                    System.out.println("Código de Genre incorrecto!");
+            } while(codigoGenre == 0);
+
+            System.out.print("Ingrese el nombre del compositor: ");
+            composerName = entrada.readLine();
+
+            track.setComposer(composerName);
+
+            System.out.print("Ingrese la duración en milisegundos: ");
+            durationTrack = Integer.parseInt(entrada.readLine());
+            
+            track.setMilliseconds(durationTrack);
+
+            System.out.print("Ingrese los bytes: ");
+            byteTrack = Integer.parseInt(entrada.readLine());
+
+            track.setBytes(byteTrack);
+
+            System.out.print("Ingrese unit price: ");
+            unitPrice = Float.parseFloat(entrada.readLine());
+
+            track.setUnitPrice(unitPrice);
+
+            em.getTransaction().begin();
+            em.persist(track);
+            em.getTransaction().commit();
+
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void cargaGenres() {
+        try {
+            Genre genre = new Genre();
+            genre.setName("Folklore");        
+            em.getTransaction().begin();
+            em.persist(genre);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void cargaMediaType() {
+        try {
+            MediaType mediaType = new MediaType();
+            mediaType.setName("Media type 1");   
+            em.getTransaction().begin();
+            em.persist(mediaType);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
